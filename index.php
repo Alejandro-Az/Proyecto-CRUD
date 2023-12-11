a6eda073355558dd2c8b208daa7f5e04144ea230
Alejandro Azuara <105530752+Alejandro-Az@users.noreply.github.com>
2023-12-10 22:47:33 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="contenedor">
+
+    <section class="menu">
+        <img src="./src/banner.jpg" alt="banner">
+       <ul>
+        <li><a href="">Uno</a></li>
+        <li><a href="">Dos</a></li>
+        <li><a href="">Tres</a></li>
+        <li><a href="">Cuatro</a></li>
+       </ul> 
+    </section>
+
+    <section>
+        <ul class="barraSuperior">
+            <li><a href="">Uno</a></li>
+            <li><a href="">Dos</a></li>
+            <li><a href="">Tres</a></li>
+            <li><a href="">Cuatro</a></li>
+        </ul>
+
+        <section class="contenidoPrincipal">
+        <h1>Contenido principal</h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad ex explicabo blanditiis voluptas voluptatem fugit ullam dolore ducimus nostrum eos nam molestiae, quibusdam quae animi. Impedit molestias delectus blanditiis commodi!
+            Provident totam accusamus illum distinctio! Repellendus veritatis quisquam illo dolor impedit consectetur maxime tempore. Tempora soluta quisquam veritatis sit hic et porro nam, omnis optio corporis sed provident incidunt neque!
+            Quo adipisci quia saepe aliquid? Esse, similique sapiente! Mollitia, perspiciatis dolor. Odio nemo consectetur commodi deleniti quo iusto enim repudiandae, laboriosam dignissimos porro suscipit saepe explicabo animi voluptas delectus ipsam.
+            Consequuntur voluptas cupiditate alias natus laudantium? Pariatur enim animi quas quos aliquam. Odio cum nobis minus neque natus perferendis aliquam ratione dolores, mollitia sapiente ea delectus maiores consectetur totam incidunt?
+            Iure illum doloremque laboriosam dolore ea quas maiores pariatur, voluptatum a! Sequi vero perspiciatis cumque, quaerat at doloribus molestiae enim velit nam mollitia quos ipsam fugit aut sit eum aliquam?
+            Totam nihil ut tempore non quae mollitia perspiciatis. Molestias aperiam maiores temporibus molestiae, minima qui ea delectus voluptatum in, officiis exercitationem facilis, pariatur atque recusandae velit? Ipsa suscipit enim soluta.
+        </p>
+    </section>
+
+    </section>
+
+    </div>
+    
+    
+    
+    
+    <script src="./main.js"></script>
+</body>
+</html>
